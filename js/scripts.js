4f5e3e06788ab6f8759f15d57f67dbd867aa4ffc
Brianca Knight <briancaknight@gmail.com>
2023-10-27 09:19:01 -0700

    Add and pass second test to replace 1s with the phrase Beep

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,13 @@ function beepBoop(number) {
   if (number < 0) {
   return "Please enter a number greater than 0"
   }
-  let result = []
+  let result = [];
   for (let i= 0; i <= number; i ++ ) {
+    if (i === 1 || i === 10 || i === 11 || i === 100) {
+      result.push("beep");
+    } else {
     result.push(i);
   }
+}
   return result;
   }
