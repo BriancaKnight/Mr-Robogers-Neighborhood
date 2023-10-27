a4636d0c897be95a3e9878305bb1606096252dca
Brianca Knight <briancaknight@gmail.com>
2023-10-27 15:57:28 -0700

    Ammend UI logic to show new results and hide old results upon resubmission.

--- js/scripts.js
+++ js/scripts.js
@@ -9,11 +9,11 @@ function beepBoop(number) {
     let numberString = i.toString();
 
     if (numberString.includes(3)) {
-      result.push("Won't you be my neighbor?");
+      result.push("'Won't you be my neighbor?'");
     } else if (numberString.includes(2)) {
-      result.push("Boop!");
+      result.push("'Boop!'");
     } else if (numberString.includes(1)) {
-      result.push("Beep!");
+      result.push("'Beep!'");
     } else {
       result.push(i);
     }
@@ -24,19 +24,23 @@ function beepBoop(number) {
 // User Interface Logic
 
 window.addEventListener("load", function () {
+  const numberInput = document.getElementById("numberInput");
+  const resultDisplay = document.getElementById("displayResults");
+  const errorDisplay = document.getElementById("displayError");
+
+
   document.getElementById("userNumber").addEventListener("submit", function (e) {
     e.preventDefault();
-    let number = document.getElementById("numberInput").value;
-    number = parseInt(number);
-    let resultDisplay = document.getElementById("displayResults");
-    let errorDisplay = document.getElementById("displayError");
-
+    let number = parseInt(numberInput.value);
     let result = beepBoop(number);
 
     if (result === "Please enter a number between 0 and 99999") {
       errorDisplay.innerText = result;
+      resultDisplay.innerText = "";
     } else {
-    resultDisplay.innerText = result.join(" ");
+      resultDisplay.innerText = result.join(", ");
+      errorDisplay.innerText = "";
     }
   });
 });
+
